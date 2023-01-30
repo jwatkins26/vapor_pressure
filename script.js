93c03210e9a01cae96c677457208fc6ef798aac6
John Watkins <johnwes87@protonmail.com>
2023-01-30 15:45:29 -0600

    Finished Equation

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ function showTemp() {
     let temp = document.getElementById('temp').value;
     let hum = document.getElementById('hum').value;
 
-    let svp = findSvp(temp);
-    let vpd = calculateVpd(newSvp, hum);
-    console.log(vpd)
+    temp = farenheitToCelsius(temp);
+    svp = findSvp(temp);
+    vpd = calculateVpd(svp, hum);
 
-    
+    let newVpd = vpd / 1000;
+
+    document.getElementById('output').value = newVpd.toFixed(2);    
+}
+
+function farenheitToCelsius(x) {
+    let celsius = (x -32) * .5556;
+    return celsius;
 }
 
 function findSvp(temp) {
-    let svp = 610.78 * 107.5 * temp/ (temp + 273.3)
-    let newSvp = svp / 1000;
-    return newSvp
+   let svp = 610.78 * Math.E**(temp / (temp + 237.3) * 17.2694);
+   return svp;
 }
 
 function calculateVpd(svp, hum) {
